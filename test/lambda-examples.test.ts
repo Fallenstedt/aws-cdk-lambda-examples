4ff4c41083c9131bf60377928a8bdc850a8d8a1a
Alex Fallenstedt <afallenstedt@newrelic.com>
2021-11-27 12:36:22 -0800

    Initial commit

---
+++ test/lambda-examples.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as LambdaExamples from '../lib/lambda-examples-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-examples-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaExamples.LambdaExamplesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
